3eaead18fb372f66370acfaf194c1cb33184b998
Ian Frost <inf5000@gmail.com>
2018-11-16 12:32:58 -0500

    Added radio field. Broke OptionList into its own component

--- src/components/RadioField/RadioField.js
+++ src/components/RadioField/RadioField.js
@@ -1,40 +1,28 @@
 import React, { Component } from "react";
 import PropTypes from 'prop-types';
+import { OptionList } from '../OptionList/OptionList'
+import { Field } from '../Field/Field';
 
 export class RadioField extends Component {
 
     render() {
+        const fieldClass = '';
+        
         return (
-            <div className={"c-field " + this.props.styleModifier }>
-
-                <h4 className="c-field__label">{ this.props.heading}</h4>
-
-                <div className="c-field__body">
-
-                    <ul className="c-option-list">
-
-                    {this.props.listItems.map(function(listItem){
-                        return (
-                            <li className="c-option-list__item">
-
-                                <label className="c-input-group" for={listItem.id}>
-                                    <input id={listItem.id} type="radio" name={listItem.name} value={ listItem.value } className="c-input-group__control"  checked={listItem.checked} disabled={listItem.disabled} readonly={listItem.readonly} />
-                                    <span className="c-input-group__text">{ listItem.text }</span>
-                                </label>
-
-                            </li>
-                        )
-                    })}
-
-                    </ul>
-
-                </div>
-
-                {this.props.fieldNote &&
-                    <div className="c-field__note">{this.props.fieldNote}</div>
-                }
-
-            </div>
+            <Field
+				className={fieldClass}
+				id={this.props.id}
+				label={this.props.label}
+				hasError={this.props.hasError}
+				disabled={this.props.disabled}
+				required={this.props.required}
+				fieldNote={ this.props.fieldNote }
+				title={ this.props.title }
+			>
+                <OptionList
+                    listItems={this.props.listItems}
+                 />
+            </Field>
         )
     }
 };
@@ -42,19 +30,28 @@ export class RadioField extends Component {
 RadioField.defaultProps = {
     listItems: [
         {
-          "id": "radio-1",
-          "name": "radioexample",
-          "text": "Radio 1"
+            "id": "radio-1",
+            "name": "radioexample",
+            "text": "Radio 1",
+            "type": "radio"
         },
         {
-          "id": "radio-2",
-          "name": "radioexample",
-          "text": "Radio 2"
+            "id": "radio-2",
+            "name": "radioexample",
+            "text": "Radio 2",
+            "type": "radio"
         },
         {
-          "id": "radio-3",
-          "name": "radioexample",
-          "text": "Radio 3"
+            "id": "radio-3",
+            "name": "radioexample",
+            "text": "Radio 3",
+            "type": "radio"
+        },
+        {
+            "id": "radio-4",
+            "name": "radioexample",
+            "text": "Radio 4",
+            "type": "radio"
         }
     ]
 }
