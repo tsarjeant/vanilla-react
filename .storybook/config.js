aea4c062a23fa8ab070e2af5c0fca0606941c55f
Brad Frost <brad@bradfrost.com>
2018-11-20 15:32:33 -0500

    Removing info for now

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,12 +1,5 @@
 import { configure, addDecorator } from "@storybook/react";
-import { withInfo } from "@storybook/addon-info";
 import "../src/css/style.css";
-
-addDecorator(
-	withInfo({
-		inline: true
-	})
-);
 
 const req = require.context("../src", true, /.stories.js$/);
 function loadStories() {
