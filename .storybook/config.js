e534eb3b4b89edff3978b2cf718b2a49e91cd59a
Brad Frost <brad@bradfrost.com>
2018-11-20 13:07:23 -0500

    Add storybook config for info

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,12 @@
-import { configure } from "@storybook/react";
+import { configure, addDecorator } from "@storybook/react";
+import { withInfo } from "@storybook/addon-info";
 import "../src/css/style.css";
+
+addDecorator(
+	withInfo({
+		inline: true
+	})
+);
 
 const req = require.context("../src", true, /.stories.js$/);
 function loadStories() {
--- src/components/InlineForm/InlineForm.stories.js
+++ src/components/InlineForm/InlineForm.stories.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { withInfo } from '@storybook/addon-info';
-import { withKnobs } from '@storybook/addon-knobs/react';
-import { withSmartKnobs } from 'storybook-addon-smart-knobs';
-import { InlineForm } from './InlineForm';
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { withInfo } from "@storybook/addon-info";
+import { withKnobs } from "@storybook/addon-knobs/react";
+import { withSmartKnobs } from "storybook-addon-smart-knobs";
+import { InlineForm } from "./InlineForm";
 
-let stories = storiesOf('Molecules/Forms/InlineForm', module);
+let stories = storiesOf("Molecules/Forms/InlineForm", module);
 
-stories.addDecorator(withInfo).addDecorator(withSmartKnobs).addDecorator(withKnobs);
+stories.addDecorator(withSmartKnobs).addDecorator(withKnobs);
 
-stories.add('Default', () =>
-  <InlineForm 
-    method="post"
-    action="http://google.com"
-    id="demo-form-id"
-    placeholder="Placeholder"
-    cta="Action" 
-  />
-);
+stories.add("Default", () => (
+	<InlineForm
+		method="post"
+		action="http://google.com"
+		id="demo-form-id"
+		placeholder="Placeholder"
+		cta="Action"
+	/>
+));
