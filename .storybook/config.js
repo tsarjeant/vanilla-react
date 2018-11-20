3f5f7ab062a5c22f75fd36ba5f96fef171bb7261
Brad Frost <brad@bradfrost.com>
2018-11-20 10:17:30 -0500

    Update react-scripts and storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,9 @@
-import { configure } from '@storybook/react';
-import '../src/css/style.css';
+import { configure } from "@storybook/react";
+import "../src/css/style.css";
 
 const req = require.context("../src", true, /.stories.js$/);
 function loadStories() {
-  req.keys().forEach(filename => req(filename));
+	req.keys().forEach(filename => req(filename));
 }
 
 configure(loadStories, module);
