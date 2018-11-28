fdee56344ad168c7e606ef9a53e003e818f14a73
Brad Frost <brad@bradfrost.com>
2018-11-28 11:05:20 -0500

    Remove readonly checkbox and radio thing

--- src/components/3.AtomsForms/Checkbox/Checkbox.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-
-export class Checkbox extends Component {
-	render() {
-		return <input name="checkbox" type="checkbox" checked="checked" />;
-	}
-}
--- src/components/3.AtomsForms/Radio/Radio.stories.js
+++
@@ -1,7 +0,0 @@
-import React, { PropTypes } from "react";
-import { storiesOf } from "@storybook/react";
-import { Radio } from "./Radio";
-
-let stories = storiesOf("Atoms/Forms/Radio", module);
-
-stories.add("Default", () => <Radio />);
